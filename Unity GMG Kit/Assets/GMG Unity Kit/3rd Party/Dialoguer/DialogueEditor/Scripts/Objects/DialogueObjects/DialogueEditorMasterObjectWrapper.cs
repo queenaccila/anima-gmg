30d3b4e999f33cf48421e1ebf62156f2676739b4
Helen Ho <helenho2k1@gmail.com>
2023-07-19 23:29:17 -0700

    Redoing unity folder

--- Unity GMG Kit/Assets/GMG Unity Kit/3rd Party/Dialoguer/DialogueEditor/Scripts/Objects/DialogueObjects/DialogueEditorMasterObjectWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace DialoguerEditor{
-	[System.Serializable]
-	public class DialogueEditorMasterObjectWrapper : ScriptableObject{
-		public DialogueEditorMasterObject data;
-	}
-}
